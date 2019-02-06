b97aaa3058a8c00bfc91817e3aa71d878a9bb5d0
Micha Alt <micha@haus23.net>
2019-02-06 19:20:53 +0100

    ng: ranking module created

---
+++ src/app/ranking/ranking.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RankingRoutingModule } from './ranking-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RankingRoutingModule
+  ]
+})
+export class RankingModule { }
