b55c881ae03b8567aef5b66557ec36e41eed3b3f
Micha Alt <micha@haus23.net>
2019-02-08 15:06:58 +0100

    Some cleanup.

--- src/app/ranking/services/championships.resolver.ts
+++ src/app/ranking/services/championships.resolver.ts
@@ -14,14 +14,7 @@ export class ChampionshipsResolver implements Resolve<Championship[]> {
   async resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<Championship[]> {
 
     const championships = await this.api.getChampionships();
-
-    if (route.firstChild.paramMap.get('slug') === null) {
-
-    }
-    // const current = championships[0];
-
     this.state.setChampionships(championships);
-    // this.state.setCurrentChampionship(current);
 
     return championships;
   }
