6592fde1b9e56288ae224e4f2ac5c404efa996e2
Micha Alt <micha@haus23.net>
2019-02-18 09:58:34 +0100

    ng: vmware clarity added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RankingModule } from './ranking/ranking.module';
+import { ClarityModule } from '@clr/angular';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { RankingModule } from './ranking/ranking.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RankingModule
+    RankingModule,
+    ClarityModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
