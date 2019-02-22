087a7dbbbc61cfbe5b5daa4f1463479d21a5f2b7
Micha Alt <micha@haus23.net>
2019-02-23 00:15:32 +0100

    Small refinements

--- src/app/shared/directives/modal-dialog.directive.ts
+++ src/app/shared/directives/modal-dialog.directive.ts
@@ -5,28 +5,28 @@ import { Directive, Input, OnChanges, SimpleChanges, ElementRef, OnInit, Output,
 })
 export class ModalDialogDirective implements OnInit, OnChanges {
 
-  @Input('appVisible')
-  visible: boolean;
+  @Input()
+  appVisible: boolean;
 
-  @Output('appVisibleChange')
-  visibleChange = new EventEmitter<boolean>();
+  @Output()
+  appVisibleChange = new EventEmitter<boolean>();
 
   constructor(private modal: ElementRef) { }
 
   ngOnInit(): void {
     this.modal.nativeElement.querySelector('.modal-background')
       .addEventListener('click', () => {
-        this.visibleChange.emit(false);
+        this.appVisibleChange.emit(false);
       });
-      this.modal.nativeElement.querySelector('.delete')
+    this.modal.nativeElement.querySelector('.delete')
       .addEventListener('click', () => {
-        this.visibleChange.emit(false);
+        this.appVisibleChange.emit(false);
       });
   }
 
 
   ngOnChanges(changes: SimpleChanges): void {
-    if (this.visible) {
+    if (this.appVisible) {
       this.modal.nativeElement.classList.add('is-active');
     } else {
       this.modal.nativeElement.classList.remove('is-active');
