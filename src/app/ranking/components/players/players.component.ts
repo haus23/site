2398a4977da1c9d7b1799898f9236f2159d87af9
Micha Alt <micha@haus23.net>
2019-03-07 12:32:08 +0100

    Displaying tips for players.

--- src/app/ranking/components/players/players.component.ts
+++ src/app/ranking/components/players/players.component.ts
@@ -5,6 +5,8 @@ import { Player } from '../../models/player';
 import { Round } from '../../models/round';
 
 import { faUsersCog } from '@fortawesome/free-solid-svg-icons';
+import { Match } from '../../models/match';
+import { Tip } from '../../models/tip';
 
 @Component({
   selector: 'ranking-players',
@@ -12,29 +14,46 @@ import { faUsersCog } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./players.component.scss']
 })
 export class PlayersComponent implements OnInit {
+
   playerCogIcon = faUsersCog;
   playerModalOn = false;
 
   allPlayers: Player[];
   player: Player;
-  allRounds: Round[];
-  selectedRound: Round;
 
-  constructor(private appState: StateService, private route: ActivatedRoute) {}
+  allRounds: Round[];
+  private _selectedRound: Round;
+
+  matches: Match[];
+
+  tips: { [matchId: number]: Tip};
+
+  constructor(private state: StateService, private route: ActivatedRoute) {}
 
   ngOnInit() {
     this.route.params.subscribe((params: { slug?: string }) => {
       if (!params.slug) {
-        this.player = this.appState.players[0];
+        this.player = this.state.players[0];
       } else {
-        this.player = this.appState.players.find(p => p.slug === params.slug);
+        this.player = this.state.players.find(p => p.slug === params.slug);
       }
+      this.tips = Object.assign({},...this.state.tips.filter( t => t.playerId === this.player.id )
+        .map( t => ({ [t.matchId]: t })));
     });
-    this.allPlayers = [...this.appState.players].sort((a, b) => {
+    this.allPlayers = [...this.state.players].sort((a, b) => {
       return a.name.localeCompare(b.name);
     });
 
-    this.allRounds = this.appState.rounds;
+    this.allRounds = this.state.rounds;
     this.selectedRound = this.allRounds.slice(-1)[0];
+  }
+
+  get selectedRound() {
+    return this._selectedRound;
+  }
+
+  set selectedRound(round: Round) {
+    this._selectedRound = round;
+    this.matches = this.state.matches.filter( m => m.roundId === round.id );
   }
 }
