ac2561a530dc92836e3f249694d0019dec464548
Micha Alt <micha@haus23.net>
2019-03-07 13:09:02 +0100

    Offline usage prepared.

--- src/app/ranking/services/state.service.ts
+++ src/app/ranking/services/state.service.ts
@@ -11,6 +11,8 @@ import { Tip } from '../models/tip';
 })
 export class StateService {
 
+  private _offline = false;
+
   championships: Championship[];
 
   currentChampionship: Championship;
@@ -19,28 +21,57 @@ export class StateService {
   matches: Match[];
   tips: Tip[];
 
-  constructor(private api: ApiService) {}
+  constructor(private api: ApiService) { }
 
   async loadChampionshipList() {
-    this.championships = await this.api.getChampionships();
+
+    try {
+      this.championships = await this.api.getChampionships();
+
+      // store for offline usage
+      localStorage.championships = JSON.stringify(this.championships);
+
+    } catch {
+
+      // put app in offline mode
+      this._offline = true;
+      this.championships = JSON.parse(localStorage.championships);
+    }
+
     return this.championships;
   }
 
   async loadChampionshipData(c: Championship) {
 
-   this.currentChampionship = c;
+    this.currentChampionship = c;
 
-    await Promise.all( [
-      this.api.getPlayers(c.id),
-      this.api.getRounds(c.id),
-      this.api.getMatches(c.id),
-      this.api.getTips(c.id)
-    ]).then( results => {
-      this.players = results[0];
-      this.rounds = results[1];
-      this.matches = results[2];
-      this.tips = results[3];
-    });
+    if (!this._offline) {
+      await Promise.all([
+        this.api.getPlayers(c.id),
+        this.api.getRounds(c.id),
+        this.api.getMatches(c.id),
+        this.api.getTips(c.id)
+      ]).then(results => {
+        this.players = results[0];
+        this.rounds = results[1];
+        this.matches = results[2];
+        this.tips = results[3];
+
+        // store for offline usage
+        localStorage.players = JSON.stringify(this.players);
+        localStorage.rounds = JSON.stringify(this.rounds);
+        localStorage.matches = JSON.stringify(this.matches);
+        localStorage.tips = JSON.stringify(this.tips);
+      });
+
+    } else {
+
+      this.players = JSON.parse(localStorage.players);
+      this.rounds = JSON.parse(localStorage.rounds);
+      this.matches = JSON.parse(localStorage.matches);
+      this.tips = JSON.parse(localStorage.tips);
+
+    }
 
     return c;
   }
