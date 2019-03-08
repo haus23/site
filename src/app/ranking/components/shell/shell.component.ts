078fd1e4180ec1cebe14bf7871eec14b86f0e7d6
Micha Alt <micha@haus23.net>
2019-03-08 10:27:15 +0100

    Notificate user of update and let them force the update.

--- src/app/ranking/components/shell/shell.component.ts
+++ src/app/ranking/components/shell/shell.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { StateService } from '../../services/state.service';
 import { Championship } from '../../models/championship';
-import { Title } from '@angular/platform-browser';
+
+import { faRedo } from '@fortawesome/free-solid-svg-icons';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'ranking-shell',
@@ -10,13 +12,23 @@ import { Title } from '@angular/platform-browser';
 })
 export class ShellComponent implements OnInit {
 
+  reloadIcon = faRedo;
+  alertVisible = false;
+
   championship: Championship;
 
-  constructor(private state: StateService) {
+  constructor(private state: StateService, private updates: SwUpdate) {
+    updates.available.subscribe( ev => {
+      this.alertVisible = true;
+    });
   }
 
   ngOnInit() {
     this.championship = this.state.currentChampionship;
+
   }
 
+  update() {
+    this.updates.activateUpdate().then(() => document.location.reload());
+  }
 }
